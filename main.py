420c2b7cb55c810c21d9176b10aa02535d92ffa3
Daniel Gut <daniel.gut9107@gmail.com>
2017-02-23 20:59:28 +0100

    Small refactor

--- main.py
+++ main.py
@@ -12,24 +12,17 @@ def main():
         videos_sizes = map(int, lines[1].split())
         endpoints = []
         current_line = 2
-        while(endpoints_count):
+        for i in range(endpoints_count):
             line = map(int, lines[current_line].split())
             e = Endpoint()
             e.datacenter_latency = line[0]
-            e.caches_latency = {}
             caches_count = line[1]
             current_line += 1
-            while(caches_count):
+            for j in range(caches_count):
                 cache_id, latency = map(int, lines[current_line].split())
                 e.cache_latency[cache_id] = latency
-                caches_count -= 1
                 current_line += 1
             endpoints.append(e)
-            endpoints_count -= 1
-
-    print endpoints[0].cache_latency[7]
-
-
 
 if __name__ == "__main__":
     main()
